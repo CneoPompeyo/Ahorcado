4011d28911e0ee0b50f0d1bdf682dfc74b265319
Leandro Berto <leandroberto2010@gmail.com>
2024-11-24 13:24:55 -0300

    added more tests perder por adivinar palabra y perder por letra

--- test_ahorcadoGUI.py
+++ test_ahorcadoGUI.py
@@ -10,7 +10,7 @@ def iniciar_aplicacion():
     """Inicia la aplicación antes de cada prueba y la cierra después."""
     print("iniciando")
     process = sp.Popen(['python', 'AhorcadoApp.py'])
-    time.sleep(3)  # Espera para que la aplicación se inicie completamente
+    time.sleep(1) #Tiempo de espera
     yield process
     process.terminate()
     process.wait()
@@ -54,4 +54,43 @@ def test_adivinar_palabra(iniciar_aplicacion):
 
     ganasteMsg = pag.locateOnScreen("ganaste.png")
 
-    assert ganasteMsg is not None
+    assert ganasteMsg is not pag.ImageNotFoundException
+
+def test_perder_por_letra(iniciar_aplicacion):
+    textInput = pag.locateOnScreen("textInput.png")
+    pag.click(pag.center(textInput))
+    pag.write("PALABRA")
+
+    startButton = pag.locateOnScreen("empezar_juego.png")
+    pag.click(pag.center(startButton))
+    
+    letraInput = pag.locateOnScreen("letra_input.png")
+    letraButton = pag.locateOnScreen("adivinar_letra.png")
+
+    for letra in "TFDGHJK":
+        pag.click(pag.center(letraInput))
+        pag.write(letra)
+        pag.click(pag.center(letraButton))
+
+    perdisteMsg = pag.locateOnScreen("perdiste.png")
+
+    assert perdisteMsg is not pag.ImageNotFoundException
+
+def test_perder_por_palabra(iniciar_aplicacion):
+    textInput = pag.locateOnScreen("textInput.png")
+    pag.click(pag.center(textInput))
+    pag.write("PALABRA")
+
+    startButton = pag.locateOnScreen('empezar_juego.png')
+    pag.click(pag.center(startButton))
+
+    palabraInput = pag.locateOnScreen("palabra_input.png")
+    pag.click(pag.center(palabraInput))
+    pag.write("ERROR")
+
+    adivinarPalabraButton = pag.locateOnScreen("adivinar_palabra.png")
+    pag.click(pag.center(adivinarPalabraButton))
+
+    perdisteMsg = pag.locateOnScreen("perdiste.png")
+
+    assert perdisteMsg is not pag.ImageNotFoundException
