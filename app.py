6417b47fb8ae016e0c04c3a12332572962ac1b61
Leandro Berto <leandroberto2010@gmail.com>
2024-12-07 22:12:34 -0300

    Agregados docstrings

--- app.py
+++ app.py
@@ -6,52 +6,69 @@ app = Flask(__name__)
 # Instancia global del juego
 juego = None
 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
-@app.route('/start', methods=['POST'])
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/start", methods=["POST"])
 def start_game():
+    "Inicia el juego"
     global juego
     data = request.get_json()
-    palabra = data.get('palabra', '').upper()
+    palabra = data.get("palabra", "").upper()
     if palabra:
         juego = Ahorcado(palabra)
-        return jsonify({'message': 'Juego iniciado', 'palabra': ' '.join(juego.getPalabraEscondida())})
-    return jsonify({'message': 'Error: No se ingresó una palabra'}), 400
+        return jsonify(
+            {
+                "message": "Juego iniciado",
+                "palabra": " ".join(juego.getPalabraEscondida()),
+            }
+        )
+    return jsonify({"message": "Error: No se ingresó una palabra"}), 400
 
-@app.route('/guess_letter', methods=['POST'])
+
+@app.route("/guess_letter", methods=["POST"])
 def guess_letter():
+    "Adivinar una letra"
     global juego
     data = request.get_json()
-    letra = data.get('letra', '').upper()
+    letra = data.get("letra", "").upper()
     if juego and letra:
         mensaje = juego.pruebaLetra(letra)
-        return jsonify({
-            'message': mensaje,
-            'palabra': ' '.join(juego.getPalabraEscondida()),
-            'vidas': 0 if juego.getVidas() == -1 else juego.getVidas() ,
-            'letrasUsadas': juego.getLetrasUsadas()
-        })
-    return jsonify({'message': 'Error: Letra no válida'}), 400
+        return jsonify(
+            {
+                "message": mensaje,
+                "palabra": " ".join(juego.getPalabraEscondida()),
+                "vidas": 0 if juego.getVidas() == -1 else juego.getVidas(),
+                "letrasUsadas": juego.getLetrasUsadas(),
+            }
+        )
+    return jsonify({"message": "Error: Letra no válida"}), 400
 
-@app.route('/guess_word', methods=['POST'])
+
+@app.route("/guess_word", methods=["POST"])
 def guess_word():
+    "Adivinar palabra"
     global juego
     data = request.get_json()
-    palabra = data.get('palabra', '').upper()
+    palabra = data.get("palabra", "").upper()
     if juego and palabra:
         mensaje = juego.verificarPalabra(palabra)
-        return jsonify({'message': mensaje})
-    return jsonify({'message': 'Error: Palabra no válida'}), 400
+        return jsonify({"message": mensaje})
+    return jsonify({"message": "Error: Palabra no válida"}), 400
 
-@app.route('/reset', methods=['POST'])
+
+@app.route("/reset", methods=["POST"])
 def reset_game():
+    "Resetear el juego"
     global juego
     if juego:
         juego.reset()
-        return jsonify({'message': 'Juego reiniciado'})
-    return jsonify({'message': 'No hay juego activo para reiniciar'}), 400
+        return jsonify({"message": "Juego reiniciado"})
+    return jsonify({"message": "No hay juego activo para reiniciar"}), 400
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True, port=5000)
--- features/environment.py
+++ features/environment.py
@@ -8,17 +8,25 @@ from selenium.webdriver.chrome.options import Options
 
 from Ahorcado import Ahorcado
 
-def esperarElemento(dr,id):
+
+def esperarElemento(dr, id):
+    "Espera a localizar el elemento"
     try:
         wait = WebDriverWait(dr, 5)  # Tiempo máximo de espera: 5 segundos
         elemento = wait.until(EC.element_to_be_clickable((By.ID, id)))
         return elemento
     except StaleElementReferenceException or TimeoutException:
-        elemento = WebDriverWait(dr, timeout=5, ignored_exceptions=(TimeoutException, StaleElementReferenceException)).until(EC.presence_of_element_located((By.ID, id)))
+        elemento = WebDriverWait(
+            dr,
+            timeout=5,
+            ignored_exceptions=(TimeoutException, StaleElementReferenceException),
+        ).until(EC.presence_of_element_located((By.ID, id)))
         print(f"El botón {id} no se pudo hacer click después de esperar 10 segundos")
         return elemento
 
+
 def iniciar_aplicacion():
+    "inicia la aplicacion"
     print("Iniciando la aplicación...")
 
     options = Options()
@@ -35,42 +43,52 @@ def iniciar_aplicacion():
     print("Aplicación iniciada correctamente.")
     return driver
 
+
 def cerrar_aplicacion(driver):
-    #Cierra la aplicación después de la prueba.
+    "Cierra la aplicación después de la prueba."
     if driver:
         driver.quit()
         print("Aplicación cerrada.")
 
-def palabraInicial(dr,palabraTest):
-    input = esperarElemento(dr,"palabra")
+
+def palabraInicial(dr, palabraTest):
+    "Input de palabra a adivinar"
+    input = esperarElemento(dr, "palabra")
     input.click()
     input.send_keys(palabraTest)
-        
-    startBoton = esperarElemento(dr,"iniciar")
+
+    startBoton = esperarElemento(dr, "iniciar")
     startBoton.click()
 
-def adivinaPalabra(dr,palabraTest):
-    # Localizar el campo de entrada para la palabra
-    input = esperarElemento(dr,"palabra-completa")
+
+def adivinaPalabra(dr, palabraTest):
+    "Localizar el campo de entrada para la palabra"
+    input = esperarElemento(dr, "palabra-completa")
     input.click()
     input.send_keys(palabraTest)
 
-    boton = esperarElemento(dr,"adivina-palabra")
+    boton = esperarElemento(dr, "adivina-palabra")
     boton.click()
 
+
 @fixture
-def before_scenario(context,scenario):
+def before_scenario(context, scenario):
+    "Iniciar aplicacion antes de cada escenario"
     context.driver = iniciar_aplicacion()
 
-def after_scenario(context,scenario):
+
+def after_scenario(context, scenario):
+    "Cerrar aplicacion luego de cada escenario"
     cerrar_aplicacion(context.driver)
 
-def before_step(context,step):
+
+def before_step(context, step):
+    "Acciones a realizar antes de cada step"
     dr = context.driver
-    if ('Reiniciar juego luego de' in context.scenario.name):
-        if (step.step_type == 'given' and 'se muestra mensaje' in step.name):
-            palabraInicial(dr,'PALABRA')
-            if ('perder' in context.scenario.name):
-                adivinaPalabra(dr,'MANZANA')
+    if "Reiniciar juego luego de" in context.scenario.name:
+        if step.step_type == "given" and "se muestra mensaje" in step.name:
+            palabraInicial(dr, "PALABRA")
+            if "perder" in context.scenario.name:
+                adivinaPalabra(dr, "MANZANA")
             else:
-                adivinaPalabra(dr,'PALABRA')
+                adivinaPalabra(dr, "PALABRA")
